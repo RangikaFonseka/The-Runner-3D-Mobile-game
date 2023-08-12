50c61239c47033547b35b2aff0d50c1deed5d0de
RangikaFonseka <122279418+RangikaFonseka@users.noreply.github.com>
2023-08-12 17:34:59 +0530

    Add files via upload

---
+++ Script/MenuManger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManger : MonoBehaviour {
+	public GameObject bannerStart;
+
+	void Start () {
+      bannerStart.GetComponent<AdmobScript> ().RequestBanner ();
+
+		bannerStart.GetComponent<AdmobScript> ().ShowBannerAD ();
+
+	}
+	
+
+	void Update () {
+		
+	}
+}
